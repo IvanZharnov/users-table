0b14b8df7a719aead9558c7178763908a27cd1b4
colinforzeal <rudenkopolina@gmail.com>
2018-11-13 00:58:25 +0300

    add modal window

--- src/resources/reducer.js
+++ src/resources/reducer.js
@@ -1,27 +1,18 @@
 import {
   GET_USERS,
   DELETE_USER,
-  SHOW_MODAL,
-  HIDE_MODAL,
 } from './action';
 
 const initialState = {
   users: [],
-  modalIsActive: false,
 }
 
 const users = (state = initialState, action) => {
   switch (action.type) {
-    case SHOW_MODAL:
-      return {...state, modalIsActive: true};
-    case HIDE_MODAL:
-      return {...state, modalIsActive: false};
     case GET_USERS:
       return action.users;
     case DELETE_USER:
-      return {
-        ...state,
-      };
+      return state.filter(item => item.id !== action.id)
     default:
       return state;
   }
